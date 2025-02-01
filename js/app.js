710028d6088ae360a47bb301c073e6daa4457d2e
JuanAmezquitaAgredo <jagredo03@gmail.com>
2025-02-01 17:15:38 -0500

    create header

--- js/app.js
+++ js/app.js
@@ -14,3 +14,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
     observer.observe(document.getElementById("app"), { childList: true, subtree: true });
 });
+
+
+// Open SideMenu
+document.getElementById('hamburger').addEventListener('click', function(event) {
+    event.stopPropagation(); 
+    sideMenu.classList.toggle('open');
+});
+
+// Close sideMenu when the user clicks
+document.addEventListener('click', function(event) {
+    const sideMenu = document.getElementById('sideMenu');
+    const hamburger = document.getElementById('hamburger');
+    
+    if (!sideMenu.contains(event.target) && !hamburger.contains(event.target)) {
+        sideMenu.classList.remove('open');
+    }
+});
+
+// Close sideMenu when select
+const menuLinks = document.querySelectorAll('#sideMenu a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        const sideMenu = document.getElementById('sideMenu');
+        sideMenu.classList.remove('open');
+    });
+});
+
